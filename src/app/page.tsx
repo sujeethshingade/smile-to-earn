8e0b23293c2e7c4d556acdcd15ef3c56a6adbd65
Sujeeth Shingade <sujeethshingade04@gmail.com>
2024-11-25 21:25:34 +0530

    feat: add camera feed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/sections/Header";
+import { Camera } from "@/sections/Camera";
 import { Footer } from "@/sections/Footer";
 
 export default function Home() {
   return (
     <>
-    <Header />
-    <Footer />
+      <Header />
+      <Camera />
+      <Footer />
     </>
   );
 }
