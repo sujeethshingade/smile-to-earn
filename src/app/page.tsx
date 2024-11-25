594e43df1b5a3a8d100dacc72c4482293dc852d4
Sujeeth Shingade <sujeethshingade04@gmail.com>
2024-11-25 19:06:07 +0530

    add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from "@/sections/Header";
+import { Footer } from "@/sections/Footer";
 
 export default function Home() {
   return (
     <>
     <Header />
+    <Footer />
     </>
   );
 }
---
+++ src/sections/Footer.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+  return (
+    <footer className={`container mx-auto py-2`}>
+      <div className={`flex justify-between items-center border border-black rounded-md p-2`}>
+        <p>&copy; 2024 Smile to Earn</p>
+        <p>Team Raptors</p>
+      </div>
+    </footer>
+  );
+};
